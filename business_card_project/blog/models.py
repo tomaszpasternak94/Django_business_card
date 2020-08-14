b2fae9c7d0be235b763139c292c49ed783d6902f
Tomek <t.pasternak94@gmail.com>
2020-08-14 20:46:35 +0200

    feat(9): a new model for the Blog part created, data from the db forwarded to /blog url

--- business_card_project/blog/models.py
+++ business_card_project/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class BlogArticles(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    description = models.TextField(max_length=300)
+
+    def __str__(self):
+        return self.title
--- business_card_project/blog/views.py
+++ business_card_project/blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import BlogArticles
 
 # Create your views here.
 
 def all_blogs(request):
-    return render(request,'blog/all_blogs.html')
+    blog_articles = BlogArticles.objects.all()
+    content = {
+        'articles' : blog_articles
+    }
+    return render(request,'blog/all_blogs.html', content)
