ea3f8d40c467e4e555a8355b307bc8da4c4e85f0
Tomek <t.pasternak94@gmail.com>
2020-08-16 14:54:49 +0200

    feat(15): format of the date field changed

--- business_card_project/blog/views.py
+++ business_card_project/blog/views.py
@@ -6,7 +6,7 @@ from .models import BlogArticles
 
 def all_blogs(request):
     #order by date (descending), limit to the last 5 in a main blog site
-    blog_articles = BlogArticles.objects.order_by('-date')[:5]
+    blog_articles = BlogArticles.objects.order_by('-date')
     content = {
         'articles' : blog_articles
     }
