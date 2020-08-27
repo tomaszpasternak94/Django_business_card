04c5a332838429f8dff248b4e293270cb64143d9
Tomek <t.pasternak94@gmail.com>
2020-08-28 01:11:19 +0200

    feat(36): visually modified form fields - in Contact Me section

--- business_card_project/portfolio/forms.py
+++ business_card_project/portfolio/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=20, required=True, label='name', widget=forms.TextInput(attrs={'placeholder':'Name'}))
-    email = forms.EmailField(required=True, label='email', widget=forms.TextInput(attrs={'placeholder':'Email'}))
-    subject = forms.CharField(max_length=100, required=True, label='subject', widget=forms.TextInput(attrs={'placeholder':'Subject'}))
-    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder':'Your message'}), required=True, label='message')
+    name = forms.CharField(max_length=20, required=True, label='name', widget=forms.TextInput(attrs={'class':'form-field-control-above', 'placeholder':'Name'}))
+    email = forms.EmailField(required=True, label='email', widget=forms.TextInput(attrs={'class':'form-field-control-above', 'placeholder':'Email'}))
+    subject = forms.CharField(max_length=100, required=True, label='subject', widget=forms.TextInput(attrs={'class':'form-field-control-above','placeholder':'Subject'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'class':'form-field-control-below','placeholder':'Your message...'}), required=True, label='message')
