cd2b2d1486b8ab8f1904895a345e54ebb79e486a
Tomek <t.pasternak94@gmail.com>
2020-09-11 00:56:11 +0200

    feat(44): Stack tech field added to Projects model. Hover overlay with stack tech on projects tiles added.

--- business_card_project/portfolio/models.py
+++ business_card_project/portfolio/models.py
@@ -5,6 +5,7 @@ class Project(models.Model):
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='portfolio/images/')
     url = models.URLField(blank=True)
+    stack = models.TextField()
 
     def __str__(self):
         return self.title
